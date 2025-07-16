ba8df5a13a3ee1677580183cb4928a9b1fe8d9cc
david-alej <david12777@outlook.com>
2025-07-16 09:08:44 -0600

    added extra seeder for seeding production environments

---
+++ 02-app/backend/src/database/seeders/99-prod-Merchant.js
@@ -0,0 +1,26 @@
+"use strict"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Merchants",
+      [
+        {
+          username: "missioneros",
+          password:
+            "$2b$10$6q.TGL9YFo6rughcema0VOf2bQIcHBjwmG1A9QbSXSMuhaZ7CEQti", //nissiJire2
+          createdAt: new Date("2024-11-11"),
+          updatedAt: new Date("2024-11-11"),
+        },
+      ],
+      {
+        ignoreDuplicates: true,
+      }
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Merchants", null, {})
+  },
+}
